773f286fbd1901c2b8b5ec629c96f9a0b5e8a0b5
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-21 21:20:34 +0000

    Update num_guess.py
    * Fixed bug where string inputs caused error
    * Fixed bug where program produced an incorrect blanket response, irrelevant of input.

--- num_guess.py
+++ num_guess.py
@@ -1,45 +1,40 @@
 import random
 
-ran_word = 0
 ran_num = random.randint(1,10)
-
-# Converts ran_num from int to str.
-if ran_num == 1:
-  ran_word = "one"
-elif ran_num == 2:
-  ran_word = "two"
-elif ran_num == 3:
-  ran_word = "three"
-elif ran_num == 4:
-  ran_word = "four"
-elif ran_num == 5:
-  ran_word = "five"
-elif ran_num == 6:
-  ran_word = "six"
-elif ran_num == 7:
-  ran_word = "seven"
-elif ran_num == 8:
-  ran_word = "eight"
-elif ran_num == 9:
-  ran_word = "nine"
-elif ran_num == 10:
-  ran_word = "ten"
 
 print "I'm thinking of a number between one and ten."
 guess = raw_input("Take a guess at what it might be: ")
 
-if type(guess) == int:
-  if guess == ran_num:
-    print "Correct."
-  elif guess < ran_num:
-    print "Wrong. Too low."
-    print "I was thinking of " + str(ran_num) + "."
-  elif guess > ran_num:
-    print "Wrong. Too high."
-    print "I was thinking of " + str(ran_num) + "."
-elif type(guess) == str:
-  if guess.lower() == ran_word:
-    print "Correct."
-  else:
-    print "Sorry, you didn't manage to guess the number I was thinking of correctly."
-  
+# Converts guess type from str to int.
+if guess == "one":
+  guess = 1
+elif guess == "two":
+  guess = 2
+elif guess == "three":
+  guess = 3
+elif guess == "four":
+  guess = 4
+elif guess == "five":
+  guess = 5
+elif guess == "six":
+  guess = 6
+elif guess == "seven":
+  guess = 7
+elif guess == "eight":
+  guess = 8
+elif guess == "nine":
+  guess = 9
+elif guess == "ten":
+  guess = 10
+
+if int(guess) == ran_num:
+  print
+  print "Correct."
+elif int(guess) < ran_num:
+  print
+  print "Ooh, too low."
+  print "I was thinking of " + str(ran_num) + "."
+elif int(guess) > ran_num:
+  print
+  print "Just a little too high."
+  print "I was thinking of " + str(ran_num) + "."
