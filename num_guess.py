5bb60d551d05c1935e4227d26a87904462eea8f2
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-22 13:55:44 +0000

    Update num_guess.py
    Fixes #3

--- num_guess.py
+++ num_guess.py
@@ -1,40 +1,51 @@
 import random
 
-ran_num = random.randint(1,10)
+play_again = True
 
-print "I'm thinking of a number between one and ten."
-guess = raw_input("Take a guess at what it might be: ")
+while play_again:
+	ran_num = random.randint(1,10)
+	
+	print "I'm thinking of a number between one and ten."
+	guess = raw_input("Take a guess at what it might be: ")
 
-# Converts guess type from str to int.
-if guess == "one":
-  guess = 1
-elif guess == "two":
-  guess = 2
-elif guess == "three":
-  guess = 3
-elif guess == "four":
-  guess = 4
-elif guess == "five":
-  guess = 5
-elif guess == "six":
-  guess = 6
-elif guess == "seven":
-  guess = 7
-elif guess == "eight":
-  guess = 8
-elif guess == "nine":
-  guess = 9
-elif guess == "ten":
-  guess = 10
+	# Converts guess type from str to int.
+	if guess == "one":
+		guess = 1
+	elif guess == "two":
+		guess = 2
+	elif guess == "three":
+		guess = 3
+	elif guess == "four":
+		guess = 4
+	elif guess == "five":
+		guess = 5
+	elif guess == "six":
+		guess = 6
+	elif guess == "seven":
+		guess = 7
+	elif guess == "eight":
+		guess = 8
+	elif guess == "nine":
+		guess = 9
+	elif guess == "ten":
+		guess = 10
 
-if int(guess) == ran_num:
-  print
-  print "Correct."
-elif int(guess) < ran_num:
-  print
-  print "Ooh, too low."
-  print "I was thinking of " + str(ran_num) + "."
-elif int(guess) > ran_num:
-  print
-  print "Just a little too high."
-  print "I was thinking of " + str(ran_num) + "."
+	if int(guess) == ran_num:
+		print
+		print "Correct."
+	elif int(guess) < ran_num:
+		print
+		print "Ooh, too low."
+		print "I was thinking of " + str(ran_num) + "."
+	elif int(guess) > ran_num:
+		print
+		print "Just a little too high."
+		print "I was thinking of " + str(ran_num) + "."
+
+	print
+
+	again = raw_input("Would you like to guess again? ")
+	if again.lower() == "no" or again.lower() == "n":
+		play_again = False
+	else:
+		print
