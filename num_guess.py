549e28463afbc2002d0703d01209d05d9c7bac16
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-23 04:57:36 +0000

    Update num_guess.py

--- num_guess.py
+++ num_guess.py
@@ -1,55 +1,47 @@
 import random
 
 play_again = True
+pick_bound = input("Would you like to pick the numbers I choose from? ").lower()
 
-print "THIS IS A STABLE RELEASE. AS SUCH, THIS PROGRAM WILL ONLY ACCEPT NUMERICAL INPUTS FOR pick_bound AND guess VARIABLES. CONTACT dev@nvai.com FOR MORE INFORMATION."
-print
+if pick_bound == "yes" or pick_bound == "y":
+  print()
+  min = input("Choose a lower bound: ")
+  max = input("Choose an upper bound: ")
+  print()
+  print("Nice pick! I'm thinking of a number between " + min + " and " + max + ".")
+else:
+  min = 1
+  max = 10
+  print()
+  print("Oh, ok then. I'm thinking of a number between one and ten.")
 
 while play_again:
-	pick_bound = True
-	while pick_bound:
-		pbound = raw_input("Would you like to pick the numbers I choose from? ")
-		if pbound.lower() == "yes" or pbound.lower() == "y":
-			pick_bound = False
-			print
-			min = raw_input("Choose a lower bound: ")
-			max = raw_input("Choose an upper bound: ")
-			
-			ran_num = random.randint(int(min),int(max))
-			
-			print
-			print "Nice pick! I'm thinking of a number between " + min + " and " + max + "."
-			guess = raw_input("Take a guess at what it might be: ")
-		else:
-			pick_bound = False
-			ran_num = random.randint(1,10)
-			print
-			print "Oh, ok then. I'm thinking of a number between one and ten."
-			guess = raw_input("Take a guess at what it might be: ")
+  ran_num = random.randint(int(min),int(max))
+  guess = input("Take a guess at what it might be: ")
+  
+  if int(guess) == ran_num:
+    print()
+    print("Correct. I was thinking of the number " + str(ran_num) + ".")
+  elif int(guess) <= ran_num - 0.5*ran_num:
+    print()
+    print("Ooh, you were way too low.")
+    print("I was thinking of the number " + str(ran_num) + ".")
+  elif int(guess) <= ran_num - 0.1*ran_num:
+    print()
+    print("You were very close - just slightly too low.")
+    print("I was thinking of the number " + str(ran_num) + ".")
+  elif int(guess) >= ran_num + 0.5*ran_num:
+    print()
+    print("Ooh, you guessed quite high!")
+    print("I was thinking of the number " + str(ran_num) + ".")
+  elif int(guess) >= ran_num + 0.1*ran_num:
+    print()
+    print("Your guess was quite close - just a little too high.")
+    print("I was thinking of the number " + str(ran_num) + ".")
 
-	if int(guess) == ran_num:
-		print
-		print "Correct. I was thinking of the number " + str(ran_num) + "."
-	elif int(guess) <= ran_num - 0.5*ran_num:
-		print
-		print "Ooh, you were way too low."
-		print "I was thinking of the number " + str(ran_num) + "."
-	elif int(guess) <= ran_num - 0.1*ran_num:
-		print
-		print "You were very close - just slightly too low."
-		print "I was thinking of the number " + str(ran_num) + "."
-	elif int(guess) >= ran_num + 0.5*ran_num:
-		print
-		print "Ooh, you guessed quite high!"
-		print "I was thinking of the number " + str(ran_num) + "."
-	elif int(guess) >= ran_num + 0.1*ran_num:
-		print
-		print "Your guess was quite close - just a little too high."
-		print "I was thinking of the number " + str(ran_num) + "."
-
-	print
+  print()
 
 	# Verifies if the user would like to re-run the code
-	again = raw_input("Would you like to guess again? ")
-	if again.lower() == "no" or again.lower() == "n":
-		play_again = False
+  again = input("Would you like to guess again? ").lower()
+  if again == "no" or again == "n":
+    play_again = False
